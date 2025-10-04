58633f9d91490d423d0d53712db6d3809c3bbd5a
MistressHorse <sofiazhguleva131@gmail.com>
2025-10-04 20:07:00 +0300

    random commit before pull

--- bot.py
+++ bot.py
@@ -1,19 +1,40 @@
 import asyncio
 import logging
 from aiogram import Bot, Dispatcher, types
-from aiogram.filters.command import Command
 from config import config
+from aiogram.filters import Command
+import random
+import traceback
 
-logging.basicConfig(level=logging.INFO)
-bot = Bot(token=config.bot_token.get_secret_value())
-dp = Dispatcher()
+try:
+    # здесь твой основной код бота
+    logging.basicConfig(level=logging.INFO)
+    bot = Bot(token=config.bot_token.get_secret_value())
+    dp = Dispatcher()
 
-@dp.message(Command("start"))
-async def cmd_start(message: types.Message):
-    await message.answer("Hello!")
+    # lines = open('ICEGERGERT.txt').readlines()
+    with open("ICEGERGERT.txt", "r", encoding="utf-8") as f:
+        lines = f.readlines()
 
-async def main():
-    await dp.start_polling(bot)
 
-if __name__ == "__main__":
-    asyncio.run(main())
+    @dp.message(Command("start"))
+    async def cmd_start(message: types.Message):
+        await message.answer("Напиши /swaga, чтобы получить строчку из песен ICEGERGERT, характеризующую твой день")
+
+
+    @dp.message(Command("swaga"))
+    async def cmd_swaga(message: types.Message):
+        line = random.choice(lines)
+        await message.answer(line)
+
+
+    async def main():
+        await dp.start_polling(bot)
+        print("Bot is activated...")
+
+
+    if __name__ == "__main__":
+        asyncio.run(main())
+except Exception:
+    print("Ошибка при запуске бота:")
+    traceback.print_exc()
